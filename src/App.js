70541eae13123cd711c8ee05fd86d3c1d29a6fdd
Caio Magalhaes <caio@arch.localdomain>
2020-07-31 17:46:13 -0300

    Add NotFound  and BottomMenu to app.js

--- src/App.js
+++ src/App.js
@@ -2,15 +2,17 @@ import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
 import Login from './pages/Login';
+import BottomMenu from './components/BottomMenu';
 import Logo from './images/copo.svg';
+import NotFound from './pages/NotFound';
 
 function App() {
   return (
     <BrowserRouter>
-      <div id="meals">
+      {/* <div id="meals">
         <span>TRYBE</span>
         <img src={Logo} alt="imageLogo" />
-      </div>
+      </div> */}
       <Switch>
         <Route exact path="/" component={Login} />
         {/* <Route exact path="/comidas" component={Comidas} /> */}
@@ -28,7 +30,9 @@ function App() {
       <Route exact path="/perfil" component={Login} />
       <Route exact path="/receitas-feitas" component={Login} />
       <Route exact path="/receitas-favoritas" component={Login} /> */}
+        <Route component={NotFound} />
       </Switch>
+      <BottomMenu />
     </BrowserRouter>
   );
 }
