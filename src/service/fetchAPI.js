3f7a438f669754388ced6635df3a423c018c7ee2
Diego Campos <diegodiko05@gmail.com>
2020-07-30 21:05:07 -0300

    Fetchs by Names, drinks and foods

--- src/service/fetchAPI.js
+++ src/service/fetchAPI.js
@@ -1,6 +1,18 @@
 const THE_MEAL_DB_API = 'https://www.themealdb.com/api/json/v1/1/';
 const THE_COCKTAIL_DB_API = 'https://www.thecocktaildb.com/api/json/v1/1/';
-https://www.themealdb.com/api/json/v1/1/search.php?s=
-export function getMealByName(Name) {
-  return fetch(`${THE_MEAL_DB_API}/search.php?s=${name}`).then((response) => response.json());
-}
+
+export const getMealByName = async (name) => {
+  const fetchUrl = `${THE_MEAL_DB_API}/search.php?s=${name}`;
+  const response = await fetch(fetchUrl);
+  const json = await response.json();
+  const data = await (response.ok ? Promise.resolve(json) : Promise.reject(json));
+  return data;
+};
+
+export const getDrinksByName = async (name) => {
+  const fetchUrl = `${THE_COCKTAIL_DB_API}/search.php?s=${name}`;
+  const response = await fetch(fetchUrl);
+  const json = await response.json();
+  const data = await (response.ok ? Promise.resolve(json) : Promise.reject(json));
+  return data;
+};
