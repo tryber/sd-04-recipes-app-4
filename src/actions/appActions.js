505f6bc158e2cb82578d65e98b05436eb52beb0a
Diego Campos <diegodiko05@gmail.com>
2020-08-05 20:57:27 -0300

    reducer para action do ID

--- src/actions/appActions.js
+++ src/actions/appActions.js
@@ -1,6 +1,12 @@
 export const APP_LOCATION = 'APP_LOCATION';
+export const ID_ATUAL = 'ID_ATUAL';
 
 export const setAppLocation = (location) => ({
   type: APP_LOCATION,
   location,
+});
+
+export const setIdAtual = (id) => ({
+  type: ID_ATUAL,
+  id,
 });
--- src/reducers/appReducers.js
+++ src/reducers/appReducers.js
@@ -1,14 +1,16 @@
-import { APP_LOCATION } from '../actions/appActions';
+import { APP_LOCATION, ID_ATUAL } from '../actions/appActions';
 
 const initialState = {
   location: 'comidas',
+  idAtual: '',
 };
 
-export default (state = initialState, { type, location }) => {
-  switch (type) {
+export default (state = initialState, action) => {
+  switch (action.type) {
     case APP_LOCATION:
-      return { ...state, location };
-
+      return { ...state, location: action.location };
+    case ID_ATUAL:
+      return { ...state, idAtual: action.id };
     default:
       return state;
   }
