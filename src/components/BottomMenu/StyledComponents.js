812e7360a2f2b51799232d3a384195c6b21359d2
Caio Magalhaes <caio@arch.localdomain>
2020-08-16 11:49:01 -0300

    Do some tweaks on bottom menu

--- src/components/BottomMenu/StyledComponents.js
+++ src/components/BottomMenu/StyledComponents.js
@@ -14,12 +14,13 @@ export const Menu = styled.div`
   height: 60px;
   justify-content: center;
   left: 0;
-  padding-bottom: 12px;
+  padding-bottom: 8px;
   padding-top: 8px;
   position: fixed;
   width: 100%;
   z-index: 999;
   font-size: 1.3em;
+  line-height: 18px;
   `;
 
 export const MenuItem = styled.div`
@@ -29,17 +30,17 @@ export const MenuItem = styled.div`
   flex-flow: column;
   flex: 1;
   font-weight: 800;
+  font-size: 18px;
   justify-content: center;
-  max-width: 168 dp;
   user-select: none;
-  transition: color 3s;
+  transition: all 3s ease-out;
 `;
 
 export const MenuIcon = styled.div`
   background-color: ${(props) => (props.isSelected ? '#8d29b3' : '#161616')};
   height: 28px;
   width: 28px;
-  margin-bottom: 5px;
+  margin-bottom: 2px;
   mask: url(${(props) => props.src}) no-repeat center;
   mask-size: contain;
 `;
